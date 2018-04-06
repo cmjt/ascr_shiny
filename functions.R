c8f3d5144a96cb8f4f2c89ac8d745a7a4107c2ed
cmjt <cmjt@st-andrews.ac.uk>
2018-04-06 19:15:37 +1200

    adding get.ascr function

--- functions.R
+++ functions.R
@@ -138,3 +138,34 @@ pkgs.ui <- function() {
       stop("To run the ascr user interface please ensure the following packages are installed: shiny, shinyjs, shinythemes, shinycssloaders, rmarkdown, animation, ascr")
   }
 }
+
+
+#' Function to get ascr package from github
+#' @export
+get.ascr <- function(){
+    pkgs <- c("CircStats", "fastGHQuad", "knitr", "matrixStats", "mvtnorm", "optimx", "plyr",
+              "Rcpp", "R2admb", "secr", "testthat", "truncnorm", "xtable", "downloader")
+    options(warn = -1)
+    for (i in pkgs){
+        if (!require(i, quietly = TRUE, character.only = TRUE)){
+            install.packages(i)
+        }
+    }
+    options(warn = 0)
+    if (.Platform$OS == "windows"){
+        bin.name <- "https://github.com/b-steve/ascr/releases/download/v2.1.1/ascr_2.1.1.zip"
+        ext <- ".zip"
+        type <- "win.binary"
+    } else if (.Platform$OS == "unix"){
+        bin.name <- "https://github.com/b-steve/ascr/archive/v2.1.1.tar.gz"
+        ext <- ".tar.gz"
+        type <- "source"
+    } else {
+        stop("Unknown OS type.")
+    }
+    dest <- paste("ascr_2.1.1", ext, sep = "")
+    library(downloader)
+    download(bin.name, destfile = dest)
+    install.packages(dest, repos = NULL, type = type)
+    unlink(dest)
+}
