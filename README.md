df1e625c2ec03081a03f9e186d26dce607c1c931
cmjt <cmjt@st-andrews.ac.uk>
2018-04-16 16:54:56 +1200

    change launch

--- README.md
+++ README.md
@@ -7,6 +7,6 @@ Repository for accompanying shiny app for the `R` package [`ascr`](https://githu
 
 To use the application online follow this [link](https://cmjt.shinyapps.io/ascr_shiny/). 
 
-To use this application locally please download this repository and extract all files into a folder. Once completed run this [script](https://raw.githubusercontent.com/cmjt/ascr_shiny/master/launch.r) to open up the application in your browser.
+To use this application locally please download this repository and extract all files into a folder. Once done you can simply open R and use this [script](https://raw.githubusercontent.com/cmjt/ascr_shiny/master/launch.r), having set `appDir <- "path/to/ascr_shiny"` to the path on your computer, to open up the application in your browser.
 
 Details on using the `ascr` shiny application can be found [here](https://cmjt.github.io/ascr_shiny/).
--- launch.r
+++ launch.r
@@ -1,23 +1,19 @@
-#' Function to launch ascr shniy application.
-#' Looks for the ascr app folder in home directory
-#' No arguments are required.
+#' Script to check required pakages for the 'ascr' shiny application are loaded
+#' and launch the application given a user defined <path/to/ascr_shiny>
 
-launch.ui <- function() {
-  appDir <- list.files("/home", "ascr_shiny", recursive=TRUE, full.names=TRUE, include.dirs=TRUE)[1]
-  if (appDir == "") {
-    stop("Looks like the ascr_shiny application is not available. Please download from https://github.com/cmjt/ascr_shiny" , call. = FALSE)
-  }
-  pkgs <- c("shiny","rmarkdown", "shinyjs", "shinycssloaders","shinythemes","animation","devtools")
-  options(warn = -1)
-  for (i in pkgs){
-      if (!require(i, quietly = TRUE, character.only = TRUE)){
-          install.packages(i)
-      }
-  }
-  if(!require("ascr",quietly = TRUE, character.only = TRUE)){
-      devtools::install_github("https://github.com/b-steve/ascr")
-  }
-  runApp(appDir, display.mode = "normal",launch.browser = TRUE)
+appDir <- "path/to/ascr_shiny" ## user should set "path/to/ascr_shiny" to the path to where the ascr_shiny folder is
+
+pkgs <- c("shiny","rmarkdown", "shinyjs", "shinycssloaders","shinythemes","animation","devtools")
+options(warn = -1)
+for (i in pkgs){
+    if (!require(i, quietly = TRUE, character.only = TRUE)){
+        install.packages(i)
+    }
 }
+if(!require("ascr",quietly = TRUE, character.only = TRUE)){
+    devtools::install_github("https://github.com/b-steve/ascr")
+}
+runApp(appDir, display.mode = "normal",launch.browser = TRUE)
+
 
 
